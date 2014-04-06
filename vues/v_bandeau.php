80996aa4fcd648ce68ac3c990abff40737539f5d
lienafalionel <lienafa.lionel@gmail.com>
2014-04-06 13:56:52 +0200

    Petites modifs

--- vues/v_bandeau.php
+++ vues/v_bandeau.php
@@ -1,5 +1,7 @@
 <!-- Bandeau -->
 <div id="header">
-	<div id="logo">
-		<h1>My Diet Fit</h1>
-	</div>
+	<a href="index.php?uc=identif&action=frmConnexion">
+		<div id="logo">
+			<h1>My Diet Fit</h1>
+		</div>
+	</a>
--- vues/v_frmSuivi.php
+++ vues/v_frmSuivi.php
@@ -3,13 +3,13 @@
 		<label class="alignLabel required">Date <span class="postscriptum">(JJ/MM/AAAA)</span></label>
 		<input name="Date" type="text" class="DatePicker" /> <br> <br>
 		
-		<label class="alignLabel required">Votre poids </label>
+		<label class="alignLabel required">Votre poids <span class="postscriptum">(en kg)</span></label>
 		<input name="poids" type="text" id="poids" /> <br> <br>
 		
-		<label class="alignLabel required">Votre taille </label>
+		<label class="alignLabel required">Votre taille <span class="postscriptum">(en cm)</span></label>
 		<input name="taille" type="text" id="taille" /> <br> <br>
 		
-		<label class="alignLabel required">Evenement :</label><br/>
+		<label class="alignLabel required">Evenement </label><br/>
 		<textarea name="Event" id="Event" rows="10" cols="50">Exemple : début régime, écart sur mon alimentation, reprise du sport, arrêt de la cigarette.</textarea> <br> <br>
 	
 	</fieldset> <br>
