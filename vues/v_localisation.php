105e0f64c2d1d7b79a396b3d664dda3bb7a007db
JeremySamLong <jeremy.samlong@gmail.com>
2014-04-10 10:25:38 +0200

    vue localisation

---
+++ vues/v_localisation.php
@@ -0,0 +1,24 @@
+<h1 align="center">Localisation </h1>
+<br>
+<br>
+<div>
+<h4> Rechercher sur un rayon de 5km</h4>
+<form action="" method="POST">
+<input type="checkbox" name="choix1" value="piscine"> piscine
+<br>
+<input type="checkbox" name="choix2" value="gymnase"> gymnase
+<br>
+<input type="checkbox" name="choix3" value="salle de muscule"> salle de musculation
+<br>
+<input type="submit" name="enregistrer" value="valider"/>
+</form>
+</div>
+
+<hr>
+
+<h2 align="center"> Resultat</h2>
+<br>
+<br>
+<p align="center"> <b>Afficher la map</b></p> 
+<br>
+	<!--<h2>Résultat</h2>!-->
