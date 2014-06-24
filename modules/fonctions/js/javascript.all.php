356842c3a261223ed0ff3ea0ea1fe6fb5c63f45a
lienafalionel <lienafa.lionel@gmail.com>
2014-06-24 16:46:10 +0200

    Ajout filtre dans gestion ex & nutrition, ajout images

--- modules/fonctions/js/javascript.all.php
+++ modules/fonctions/js/javascript.all.php
@@ -5,5 +5,6 @@ $ParentPath = "../../../";
 require $ParentPath."config.inc.php";
 
 //Inclusion des fonctions javascript
+include "generale.inc.php"; echo $BR;
 include "suppression.inc.php"; echo $BR;
 ?>
